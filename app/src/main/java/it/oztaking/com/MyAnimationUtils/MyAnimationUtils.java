79167d1f96085d74447145473833bd6f4db4cf93
wsxingjun <1185889308@qq.com>
2018-01-08 09:37:12 +0800

    v4-完成优酷菜单的所有的功能

--- app/src/main/java/it/oztaking/com/MyAnimationUtils/MyAnimationUtils.java
+++ app/src/main/java/it/oztaking/com/MyAnimationUtils/MyAnimationUtils.java
@@ -1,5 +1,6 @@
 package it.oztaking.com.MyAnimationUtils;
 
+import android.text.Layout;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
@@ -10,25 +11,66 @@ import android.widget.RelativeLayout;
 
 public class MyAnimationUtils {
 
+    public  static int runningAnimationCount = 0;
+
     //旋转出去的动画
-    public static void rotateOutAnim(RelativeLayout rl){
+    public static void rotateOutAnim(RelativeLayout rl, int delay){
+        int childCount  = rl.getChildCount();
+        System.out.println("rl.getChildCount():"+childCount);
+        //如果隐藏，则将所有的子View禁用；
+        for (int i=0; i < childCount; i++){
+            rl.getChildAt(i).setEnabled(false);
+        }
+
         RotateAnimation ra = new RotateAnimation(
                 0f, -180f,
                 Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 1.0f);
         ra.setFillAfter(true);
         ra.setDuration(500);
+        ra.setStartOffset(delay);//设置动画开始延时
+        ra.setAnimationListener(new MyAnimationListener()); //添加监听
+        
         rl.startAnimation(ra);
     }
 
     //旋转进来的动画
-    public static void rotateInAnim(RelativeLayout rl){
+    public static void rotateInAnim(RelativeLayout rl, int delay){
+
+        int childCount  = rl.getChildCount();
+        System.out.println("rl.getChildCount():"+childCount);
+        //如果隐藏，则将所有的子View禁用；
+        for (int i=0; i < childCount; i++){
+            rl.getChildAt(i).setEnabled(true);
+        }
         RotateAnimation ra = new RotateAnimation(
                 -180f,0f,
                 Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 1.0f);
         ra.setFillAfter(true);
         ra.setDuration(500);
+        ra.setStartOffset(delay);
+        ra.setAnimationListener(new MyAnimationListener()); //添加监听
         rl.startAnimation(ra);
+    }
+
+    private static class MyAnimationListener implements Animation.AnimationListener {
+
+
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+            runningAnimationCount++;
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            runningAnimationCount--;
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
--- app/src/main/java/it/oztaking/com/youkumenu/MainActivity.java
+++ app/src/main/java/it/oztaking/com/youkumenu/MainActivity.java
@@ -3,6 +3,8 @@ package it.oztaking.com.youkumenu;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
@@ -20,8 +22,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private boolean isLevel3Display = true;
     private boolean isLevel2Display = true;
+    private boolean isLevel1Display = true;
 
-    private Context mContext = this;
+
 
 
     @Override
@@ -52,26 +55,33 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
+        //如果有动画执行，则直接返回
+        if (MyAnimationUtils.runningAnimationCount > 0){
+            return;
+        }
 
         switch (v.getId()) {
             case R.id.ib_home:
                 if (isLevel2Display) {
+                    int delay = 0;
                     //如果level3没有转出去，先让level3转出去
                     if (isLevel3Display){
-                        MyAnimationUtils.rotateOutAnim(rl_level3);
+                        MyAnimationUtils.rotateOutAnim(rl_level3,delay);
+                        isLevel3Display = false;
                     }
-                    MyAnimationUtils.rotateOutAnim(rl_level2);
+                    delay += 200;
+                    MyAnimationUtils.rotateOutAnim(rl_level2,delay);
                 } else {
-                    MyAnimationUtils.rotateInAnim(rl_level2);
+                    MyAnimationUtils.rotateInAnim(rl_level2,0);
                 }
                 isLevel2Display = !isLevel2Display;
                 break;
 
             case R.id.ib_menu:
                 if (isLevel3Display) {
-                    MyAnimationUtils.rotateOutAnim(rl_level3);
+                    MyAnimationUtils.rotateOutAnim(rl_level3,0);
                 } else {
-                    MyAnimationUtils.rotateInAnim(rl_level3);
+                    MyAnimationUtils.rotateInAnim(rl_level3,0);
                 }
                 isLevel3Display = !isLevel3Display;
                 break;
@@ -79,5 +89,47 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 break;
 
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        System.out.println("AAA:keycode = "+keyCode);
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            if (MyAnimationUtils.runningAnimationCount > 0){
+                return true;
+            }
+            //如果按下的是菜单按钮
+            if (isLevel1Display){
+                int delay = 0;
+                //隐藏三级菜单
+                if (isLevel3Display){
+                    MyAnimationUtils.rotateOutAnim(rl_level3,0);
+                    isLevel3Display = false;
+                    delay += 200;
+                }
+                //隐藏二级菜单
+                if (isLevel2Display){
+                    MyAnimationUtils.rotateOutAnim(rl_level2,delay);
+                    isLevel3Display = false;
+                    delay += 200;
+                }
+
+                //隐藏一级菜单
+                MyAnimationUtils.rotateOutAnim(rl_level1,delay);
+            }else {
+                MyAnimationUtils.rotateInAnim(rl_level1,0);
+                MyAnimationUtils.rotateInAnim(rl_level2,200);
+                MyAnimationUtils.rotateInAnim(rl_level3,400);
+
+                isLevel3Display = true;
+                isLevel2Display = true;
+            }
+            isLevel1Display = !isLevel1Display;
+
+            return true;//消费了当前事件
+        }
+
+
+        return super.onKeyDown(keyCode, event);
     }
 }
